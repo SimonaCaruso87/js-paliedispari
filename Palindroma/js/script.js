e0b457165c242dedc6eae0db09743be4c7bf76fb
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-06-20 22:56:28 +0200

    Add prompt gioco pali

---
+++ Palindroma/js/script.js
@@ -0,0 +1,17 @@
+/*
+Palidroma
+Chiedere all'utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+//1)Chiedere all'utente di inserire una parola
+
+const enterAworld = prompt("Inserisci una parola palindroma");
+
+console.log(enterAworld , "Inserire una parola palindroma" , typeof enterAworld);
+
+//2)Creare una funzione per capire se la parola inserità è palindroma
+
+//Se la parola inserità è palindroma vai avanti 
+//Se la parola inserita non è palindroma ricomincia 
+
