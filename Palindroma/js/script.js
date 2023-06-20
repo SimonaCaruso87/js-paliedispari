190cfe9ad82b1c6fcf42932357f1a6e707135e29
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-06-20 23:24:11 +0200

    Add list word palindrome

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -13,5 +13,26 @@ console.log(enterAworld , "Inserire una parola palindroma" , typeof enterAworld)
 //2)Creare una funzione per capire se la parola inserità è palindroma
 
 //Se la parola inserità è palindroma vai avanti 
+
+const listPalindromi = [
+    "kayak" , 
+    "aerea" ,
+    "radar" , 
+    "esse" , 
+    "emme" , 
+    "elle" , 
+    "ebbe" , 
+    "enne" , 
+    "osso" ,
+    "otto" ,
+];
+
+console.log(listPalindromi , "LIsta Parole Palindrome" , typeof listaPalindromi);
+
+
+
+// while(){
+
+// }
 //Se la parola inserita non è palindroma ricomincia 
 
