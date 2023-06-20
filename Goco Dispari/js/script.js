4f354783aa76a5508052fc87d1f5fc2ca9c600e2
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-06-21 00:12:03 +0200

    End Exercise

--- Goco Dispari/js/script.js
+++ Goco Dispari/js/script.js
@@ -1,3 +1,14 @@
+/*Pari e Dispari
+L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+Consigli del giorno
+1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+*/
+
 function game() 
 {
     let numero_user = document.getElementById('user-num').value;
